893983b90e595833022b3cced90cc01ab8b5e14d
David <davidkueng@gmail.com>
2019-02-17 18:15:22 +0100

    added assignement note

--- pong/main.lua
+++ pong/main.lua
@@ -394,3 +394,11 @@ function displayFPS()
     love.graphics.setColor(0, 255, 0, 255)
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
 end
+
+
+
+-- ASSIGNEMENT: 
+-- Implement an AI-controlled paddle (either the left or the right will do) such that it will try to deflect the ball at all times. 
+-- Since the paddle can move on only one axis (the Y axis), you will need to determine how to keep the paddle moving in relation to the ball. 
+-- Currently, each paddle has its own chunk of code where input is detected by the keyboard; this feels like an excellent place to put the code we need! 
+-- Once either the left or right paddle (or both, if desired) try to deflect the paddle on their own, you’ve done it!
