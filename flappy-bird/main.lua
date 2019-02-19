0c905ad35e1ca50776e56b36cc3787fd1427efcb
David <davidkueng@gmail.com>
2019-02-19 16:35:48 +0100

    added assignements

--- flappy-bird/main.lua
+++ flappy-bird/main.lua
@@ -12,6 +12,24 @@
     for years prior. Illustrates some of the most basic procedural generation of game
     levels possible as by having pipes stick out of the ground by varying amounts, acting
     as an infinitely generated obstacle course for the player.
+
+    ASSIGNEMENT: 
+
+    - Randomize the gap between pipes (vertical space), such that they’re no longer hardcoded to 90 pixels.
+    - Randomize the interval at which pairs of pipes spawn, such that they’re no longer always 2 seconds apart.
+    - When a player enters the ScoreState, award them a “medal” via an image displayed along with the score; this 
+      can be any image or any type of medal you choose (e.g., ribbons, actual medals, trophies, etc.), so long as 
+      each is different and based on the points they scored that life. Choose 3 different ones, as well as the minimum 
+      score needed for each one (though make it fair and not too hard to test :)).
+    - Implement a pause feature, such that the user can simply press “P” (or some other key) and pause the state of the game. 
+      This pause effect will be slightly fancier than the pause feature we showed in class, though not ultimately that much different. 
+      When they pause the game, a simple sound effect should play (I recommend testing out bfxr for this, as seen in Lecture 0!). At the same 
+      time this sound effect plays, the music should pause, and once the user presses P again, the gameplay and the music should resume just as they were! 
+      To cap it off, display a pause icon in the middle of the screen, nice and large, so as to make it clear the game is paused.
+    - love.mousepressed(x,y,button) Callback fired every time a mouse button is pressed.
+      make the function global like keypressed to track in Bird.lua file instead of main.lua.
+
+
 ]]
 
 -- push is a library that will allow us to draw our game at a virtual
